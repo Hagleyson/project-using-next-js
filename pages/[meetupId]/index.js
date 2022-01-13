7e009d224934a1b12188aae098587c48bd672504
hagleyson <hagleyson@gmail.com>
2022-01-13 12:43:46 -0300

    Adding Custom Components & CSS Modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetais() {
+  return (
+    <MeetupDetail
+      image="https://www.viajali.com.br/wp-content/uploads/2016/09/brasil-1.jpg"
+      title="First Meetup"
+      address="Some Street 5"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetais;
